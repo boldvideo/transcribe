539989f4350239460d52797f107b6fb9b21206a5
Marcel Fahle <m.fahle@gmail.com>
2023-11-19 09:59:42 +0100

    refactors transcript callback, sets missing status

--- app/controllers/transcription_callbacks_controller.rb
+++ app/controllers/transcription_callbacks_controller.rb
@@ -11,20 +11,21 @@ class TranscriptionCallbacksController < ApplicationController
        s3_client = Aws::S3::Resource.new
       bucket = s3_client.bucket('bold-transcriber')
 
-      # Save JSON
       json_key = "transcripts/#{video.id}/transcript.json"
-      bucket.object(json_key).put(body: params.to_json)
-      video.update(json_transcription_url: bucket.object(json_key).public_url)
-
-      # Save WebVTT
       vtt_key = "transcripts/#{video.id}/transcript.vtt"
-      bucket.object(vtt_key).put(body: TranscriptionConverter.to_webvtt(params))
-      video.update(webvtt_url: bucket.object(vtt_key).public_url)
-
-      # Save SRT
       srt_key = "transcripts/#{video.id}/transcript.srt"
+
+      bucket.object(json_key).put(body: params.to_json)
+      bucket.object(vtt_key).put(body: TranscriptionConverter.to_webvtt(params))
       bucket.object(srt_key).put(body: TranscriptionConverter.to_srt(params))
-      video.update(srt_url: bucket.object(srt_key).public_url)
+
+      # Update the video record with all URLs and transcription status
+      video.update(
+        json_transcription_url: bucket.object(json_key).public_url,
+        webvtt_url: bucket.object(vtt_key).public_url,
+        srt_url: bucket.object(srt_key).public_url,
+        transcription_status: 'ready'
+      )
 
     else
       logger.error("Utterances missing in transcription: #{params}")
