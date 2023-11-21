fad3323db853fc880f34dcb949b12a559fe72a7e
Marcel Fahle <m.fahle@gmail.com>
2023-11-21 06:55:10 +0100

    deployment updates

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,12 @@
 aws_cred = Rails.application.credentials.dig(:aws)
-Rails.logger.debug "AWS Credentials: #{aws_cred.inspect}"
 
-Aws.config.update({
-  region: 'eu-central-1',
-  credentials: Aws::Credentials.new(aws_cred['access_key_id'], aws_cred['secret_access_key'])
-})
+if aws_cred
+  Rails.logger.debug "AWS Credentials: #{aws_cred.inspect}"
+
+  Aws.config.update({
+    region: 'eu-central-1',
+    credentials: Aws::Credentials.new(aws_cred['access_key_id'], aws_cred['secret_access_key'])
+  })
+else
+  Rails.logger.warn "AWS credentials not found. AWS is not configured."
+end
